a71e2b08a66cc87174a5c8a04da8322c84e72784
junnys6018 <junkanglim1@gmail.com>
2021-11-01 18:36:21 +0800

    made footer responsive

--- frontend/src/components/footer/footer.tsx
+++ frontend/src/components/footer/footer.tsx
@@ -8,12 +8,12 @@ export default function Footer() {
         backgroundSize: '64px',
     };
     return (
-        <div className="h-32 flex-shrink-0" style={footerImage}>
-            <div className="container h-full flex items-center">
-                <span className="font-bold text-xl mr-20">Emulator Hub</span>
-                <a className="text-sm mr-12">Source Code</a>
-                <a className="text-sm mr-auto">Contact</a>
-                <span className="text-sm">Designed &amp; Built by Jun Lim</span>
+        <div className="lg:h-32 flex-shrink-0" style={footerImage}>
+            <div className="container h-full flex lg:items-center flex-col lg:flex-row">
+                <span className="font-bold text-xl my-12 mr-20">Emulator Hub</span>
+                <a className="text-sm mb-7 lg:mb-0 mr-12">Source Code</a>
+                <a className="text-sm mb-7 lg:mb-0 mr-auto">Contact</a>
+                <span className="text-sm mb-12 lg:mb-0">Designed &amp; Built by Jun Lim</span>
             </div>
         </div>
     );
