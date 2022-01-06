f66bb9e73e98f20b5db1bfd302adcb16e44475e0
junnys6018 <junkanglim1@gmail.com>
2022-01-06 14:21:23 +0800

    404 page: implemented 404 page

--- frontend/src/components/not-found/not-found.tsx
+++ frontend/src/components/not-found/not-found.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 function NotFound() {
-    return <p>this is a custom 404 page coming from react</p>;
+    return (
+        <div className="text-center" style={{ marginTop: '20vh', marginBottom: '20vh' }}>
+            <p className="font-semibold text-6xl md:text-7xl mb-8 md:mb-11">Oops</p>
+            <p className="font-medium text-3xl md:text-4xl mb-4 md:mb-5">Something Went Wrong</p>
+            <p className="font-normal text-xl md:text-2xl">Error 404 Page Not Found</p>
+        </div>
+    );
 }
 
 export default NotFound;
