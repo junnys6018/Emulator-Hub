e81fd5c86d1d89bee1ca079bea0dd6b878846971
junnys6018 <junkanglim1@gmail.com>
2022-01-14 13:32:28 +0800

    style: added nav to 404 page

--- frontend/src/components/not-found/not-found.test.tsx
+++ frontend/src/components/not-found/not-found.test.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import NotFound from './not-found';
+import { MemoryRouter } from 'react-router';
+
+jest.mock('@/src/storage/user-data', () => {
+    const useUserProfile = jest.fn();
+    useUserProfile.mockReturnValueOnce([
+        {
+            userName: 'Jun Lim',
+            profileImage: 'mock image',
+        },
+    ]);
+    return {
+        __esModule: true,
+        useUserProfile,
+    };
+});
 
 test('<NotFound /> snapshot', () => {
-    const tree = renderer.create(<NotFound />).toJSON();
+    const tree = renderer
+        .create(
+            <MemoryRouter>
+                <NotFound />
+            </MemoryRouter>,
+        )
+        .toJSON();
     expect(tree).toMatchSnapshot();
 });
--- frontend/src/components/not-found/not-found.tsx
+++ frontend/src/components/not-found/not-found.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import { useUserProfile } from '@/src/storage/user-data';
+import React, { Fragment } from 'react';
+import Navbar from '../util/navbar';
 
 function NotFound() {
+    const [{ userName, profileImage }] = useUserProfile();
+
     return (
-        <div className="text-center" style={{ marginTop: '20vh', marginBottom: '20vh' }}>
-            <p className="font-semibold text-6xl md:text-7xl mb-8 md:mb-11">Oops</p>
-            <p className="font-medium text-gray-300 text-3xl md:text-4xl mb-4 md:mb-5">Something Went Wrong</p>
-            <p className="font-normal text-gray-300 text-xl md:text-2xl">Error 404 Page Not Found</p>
-        </div>
+        <Fragment>
+            <Navbar userName={userName} profileImage={profileImage} />
+            <div className="text-center" style={{ marginTop: '20vh', marginBottom: '20vh' }}>
+                <p className="font-semibold text-6xl md:text-7xl mb-8 md:mb-11">Oops</p>
+                <p className="font-medium text-gray-300 text-3xl md:text-4xl mb-4 md:mb-5">Something Went Wrong</p>
+                <p className="font-normal text-gray-300 text-xl md:text-2xl">Error 404 Page Not Found</p>
+            </div>
+        </Fragment>
     );
 }
 
