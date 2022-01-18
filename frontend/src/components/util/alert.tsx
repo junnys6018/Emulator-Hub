2475c7223c8f1dcee39e5efddf9106e968f48d9f
junnys6018 <junkanglim1@gmail.com>
2022-01-18 22:45:19 +0800

    alert: Redid implementation
    Now takes in any react node as a child to render.

--- frontend/src/components/util/alert.tsx
+++ frontend/src/components/util/alert.tsx
@@ -5,14 +5,6 @@ import React, { Fragment, useCallback, useContext, useEffect, useRef, useState }
 type Severity = 'SUCCESS' | 'INFO' | 'WARN' | 'ERROR';
 type Action = 'CLOSE' | 'REFRESH' | 'CONFIRM';
 
-interface AlertProps {
-    title: string;
-    message: string;
-    severity: Severity;
-    action: Action;
-    callback?: (action: 'YES' | 'NO') => void;
-}
-
 const colorMap = {
     SUCCESS: 'bg-green-500 md:hover:bg-green-400 active:bg-green-400',
     WARN: 'bg-yellow-500 md:hover:bg-yellow-400 active:bg-yellow-400',
@@ -27,27 +19,135 @@ const borderColorMap = {
     INFO: '',
 };
 
-export function Alert(props: AlertProps) {
-    const alert = useAlert();
+interface AlertContentProps {
+    title: string;
+    message: string;
+    severity: Severity;
+    children: React.ReactNode;
+}
 
+function AlertContent(props: AlertContentProps) {
+    return (
+        <div
+            className={`alert-builtin ${
+                props.severity !== 'INFO' ? `border-t-4 ${borderColorMap[props.severity]}` : ''
+            }`}
+        >
+            <h2
+                className="text-2xl font-bold py-5 md:py-8 truncate"
+                style={{ maxWidth: '500px', overflowX: 'clip', overflowY: 'initial' }}
+            >
+                {props.title}
+            </h2>
+            <p className="mx-3 md:mx-14 text-center">{props.message}</p>
+            {props.children}
+        </div>
+    );
+}
+
+interface AlertCloseProps {
+    title: string;
+    message: string;
+    severity: Severity;
+    close: () => void;
+}
+
+function AlertClose(props: AlertCloseProps) {
     const color = colorMap[props.severity];
     const buttonType = props.severity === 'INFO' ? 'btn-secondary' : 'btn-primary';
     const buttonClass = classNames(buttonType, color);
 
+    return (
+        <AlertContent title={props.title} severity={props.severity} message={props.message}>
+            <button className={classNames('text-xl font-medium py-1.5 px-4 my-8', buttonClass)} onClick={props.close}>
+                Close
+            </button>
+        </AlertContent>
+    );
+}
+
+interface AlertRefreshProps {
+    title: string;
+    message: string;
+    severity: Severity;
+}
+
+function AlertRefresh(props: AlertRefreshProps) {
+    const color = colorMap[props.severity];
+    const buttonType = props.severity === 'INFO' ? 'btn-secondary' : 'btn-primary';
+    const buttonClass = classNames(buttonType, color);
+
+    return (
+        <AlertContent title={props.title} severity={props.severity} message={props.message}>
+            <button
+                className={classNames('text-xl font-medium py-1.5 px-4 my-8', buttonClass)}
+                onClick={() => window.location.reload()}
+            >
+                Refresh
+            </button>
+        </AlertContent>
+    );
+}
+
+interface AlertConfirmProps {
+    title: string;
+    message: string;
+    severity: Severity;
+    close: () => void;
+    callback?: (action: 'YES' | 'NO') => void;
+}
+
+function AlertConfirm(props: AlertConfirmProps) {
+    const color = colorMap[props.severity];
+    const buttonType = props.severity === 'INFO' ? 'btn-secondary' : 'btn-primary';
+    const buttonClass = classNames(buttonType, color);
+
+    return (
+        <AlertContent title={props.title} severity={props.severity} message={props.message}>
+            <div className="flex justify-center my-8">
+                <button
+                    className={classNames('text-xl font-medium py-1.5 w-20 sm:w-32 mr-8', buttonClass)}
+                    onClick={() => {
+                        if (props.callback) {
+                            props.callback('YES');
+                        }
+                        props.close();
+                    }}
+                >
+                    Yes
+                </button>
+                <button
+                    className="text-xl font-medium py-1.5 w-20 sm:w-32 btn-secondary muted"
+                    onClick={() => {
+                        if (props.callback) {
+                            props.callback('NO');
+                        }
+                        props.close();
+                    }}
+                >
+                    No
+                </button>
+            </div>
+        </AlertContent>
+    );
+}
+
+interface AlertProps {
+    children: (close: () => void) => React.ReactNode;
+}
+
+function Alert(props: AlertProps) {
+    const alert = useAlert();
+
     const container = useRef<HTMLDivElement>(null);
     const backdrop = useRef<HTMLDivElement>(null);
 
-    const close = () => {
+    const close = useCallback(() => {
         (container.current as HTMLDivElement).classList.add('hide');
         (backdrop.current as HTMLDivElement).classList.add('opacity-0');
         (backdrop.current as HTMLDivElement).classList.remove('opacity-50');
-
         (container.current as HTMLDivElement).addEventListener('transitionend', () => alert(), { once: true });
-    };
-
-    const refresh = () => {
-        location.reload();
-    };
+    }, [alert]);
 
     useEffect(() => {
         (container.current as HTMLDivElement).classList.remove('hide');
@@ -57,58 +157,8 @@ export function Alert(props: AlertProps) {
 
     return (
         <Fragment>
-            <div
-                className={`alert hide ${
-                    props.severity !== 'INFO' ? `border-t-4 ${borderColorMap[props.severity]}` : ''
-                }`}
-                ref={container}
-            >
-                <h2
-                    className="text-2xl font-bold py-5 md:py-8 truncate"
-                    style={{ maxWidth: '500px', overflowX: 'clip', overflowY: 'initial' }}
-                >
-                    {props.title}
-                </h2>
-                <p className="mx-3 md:mx-14 text-center">{props.message}</p>
-                {props.action === 'CLOSE' && (
-                    <button className={classNames('text-xl font-medium py-1.5 px-4 my-8', buttonClass)} onClick={close}>
-                        Close
-                    </button>
-                )}
-                {props.action === 'REFRESH' && (
-                    <button
-                        className={classNames('text-xl font-medium py-1.5 px-4 my-8', buttonClass)}
-                        onClick={refresh}
-                    >
-                        Refresh
-                    </button>
-                )}
-                {props.action === 'CONFIRM' && (
-                    <div className="flex justify-center my-8">
-                        <button
-                            className={classNames('text-xl font-medium py-1.5 w-20 sm:w-32 mr-8', buttonClass)}
-                            onClick={() => {
-                                if (props.callback) {
-                                    props.callback('YES');
-                                }
-                                close();
-                            }}
-                        >
-                            Yes
-                        </button>
-                        <button
-                            className="text-xl font-medium py-1.5 w-20 sm:w-32 btn-secondary muted"
-                            onClick={() => {
-                                if (props.callback) {
-                                    props.callback('NO');
-                                }
-                                close();
-                            }}
-                        >
-                            No
-                        </button>
-                    </div>
-                )}
+            <div className="alert hide" ref={container}>
+                {props.children(close)}
             </div>
             <div
                 className="fixed z-40 top-0 left-0 w-screen h-screen bg-black transition-opacity ease-linear opacity-0 select-none"
@@ -136,7 +186,7 @@ export const AlertContext = React.createContext<((message?: string, options?: Pa
 export function AlertProvider(props: AlertProviderProps) {
     const [currentAlert, setCurrentAlert] = useState<JSX.Element | null>(null);
 
-    // FIXME: if multiple called are made to `alert`, the body lock count will be out of sync causing the page to be unscrollable
+    // FIXME: if multiple calls are made to `alert`, the body lock count will be out of sync causing the page to be unscrollable
     const alert = useCallback((message?: string, options?: Partial<AlertOptions>) => {
         if (message !== undefined) {
             const defaults: AlertOptions = {
@@ -145,9 +195,53 @@ export function AlertProvider(props: AlertProviderProps) {
                 action: 'CLOSE',
             };
 
-            options = { ...defaults, ...options };
+            const resolvedOptions: AlertOptions = { ...defaults, ...options };
             lockBodyScrolling();
-            setCurrentAlert(<Alert message={message} {...(options as AlertOptions)} />);
+
+            switch (resolvedOptions.action) {
+                case 'CLOSE':
+                    setCurrentAlert(
+                        <Alert>
+                            {close => (
+                                <AlertClose
+                                    message={message}
+                                    title={resolvedOptions.title}
+                                    severity={resolvedOptions.severity}
+                                    close={close}
+                                />
+                            )}
+                        </Alert>,
+                    );
+                    break;
+                case 'CONFIRM':
+                    setCurrentAlert(
+                        <Alert>
+                            {close => (
+                                <AlertConfirm
+                                    message={message}
+                                    title={resolvedOptions.title}
+                                    severity={resolvedOptions.severity}
+                                    close={close}
+                                    callback={resolvedOptions.callback}
+                                />
+                            )}
+                        </Alert>,
+                    );
+                    break;
+                case 'REFRESH':
+                    setCurrentAlert(
+                        <Alert>
+                            {() => (
+                                <AlertRefresh
+                                    message={message}
+                                    title={resolvedOptions.title}
+                                    severity={resolvedOptions.severity}
+                                />
+                            )}
+                        </Alert>,
+                    );
+                    break;
+            }
         } else {
             unlockBodyScrolling();
             setCurrentAlert(null);
