9c9d3627f29903b3b27bf8837fc63fded846296a
junnys6018 <junkanglim1@gmail.com>
2022-01-28 13:11:08 +0800

    tweaked styles and updated snapshots

--- frontend/src/components/util/buttons.tsx
+++ frontend/src/components/util/buttons.tsx
@@ -10,9 +10,9 @@ const SettingsButtons = ({
     cancel,
 }: Pick<SettingsComponentProps<never>, requiredProps>) => {
     return (
-        <div className="flex mt-auto mb-12 lg:mb-24">
+        <div className="flex items-center mt-auto mb-12 lg:mb-24">
             <button
-                className={`btn-primary h-10 w-40 xl:w-52 lg:mr-20 ${settingsChanged() ? '' : 'disabled'}`}
+                className={`btn-primary h-10 w-40 xl:w-52 mr-10 lg:mr-20 ${settingsChanged() ? '' : 'disabled'}`}
                 onClick={onSave}
             >
                 Save
@@ -20,14 +20,16 @@ const SettingsButtons = ({
             {cancel && (
                 <button
                     className={`btn-tertiary h-10 w-40 xl:w-52 lg:mr-20 ${
-                        settingsChanged() ? 'ring-gray-50 hover:ring-red-500' : 'cursor-not-allowed ring-gray-300'
+                        settingsChanged()
+                            ? 'ring-gray-50 hover:ring-red-500 hover:text-red-500'
+                            : 'cursor-not-allowed ring-gray-500 text-gray-500'
                     }`}
                     onClick={cancel}
                 >
                     Cancel
                 </button>
             )}
-            <button className="h-10 w-auto text-gray-300 hover:text-gray-50" onClick={resetAll}>
+            <button className="text-gray-300 hover:text-gray-50" onClick={resetAll}>
                 Reset All
             </button>
         </div>
