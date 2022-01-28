796557e0296c3292987e13fb939354b88eba8f24
junnys6018 <junkanglim1@gmail.com>
2022-01-28 13:18:10 +0800

    made under construction page responsive

--- frontend/src/components/under-construction/under-construction.tsx
+++ frontend/src/components/under-construction/under-construction.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 export default function UnderConstruction() {
     return (
         <div className="flex-grow flex flex-col justify-center text-center">
-            <p className="font-semibold text-6xl md:text-7xl mb-8 md:mb-11">🚧Under Construction🚧</p>
+            <p className="font-semibold text-xl xs:text-2xl sm:text-4xl md:text-6xl lg:text-7xl mb-2 md:mb-11">
+                🚧Under Construction🚧
+            </p>
             <p className="font-medium text-gray-300 text-3xl md:text-4xl mb-4 md:mb-5">
                 This emulator is still in the works
             </p>
-            <p className="font-normal text-gray-300 text-xl md:text-2xl">Come back in a few months :)</p>
+            <p className="font-normal text-gray-300 text-xl md:text-2xl">Come back in a few months</p>
         </div>
     );
 }
